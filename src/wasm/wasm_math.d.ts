e972eaa3b67daa5abe478d0180814613a0ec9d95
idk-mohit <mototurbo12499@gmai.com>
2025-07-07 17:50:35 +0530

    Add TypeScript definitions for WebAssembly module wasm_math_bg.wasm
    - Created a new declaration file `wasm_math_bg.wasm.d.ts` to provide TypeScript support for the WebAssembly module.
    - Exported memory, functions, and necessary bindings for better integration with TypeScript.

---
+++ src/wasm/wasm_math.d.ts
@@ -0,0 +1,48 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * Alerts "Hello, wasm!" in the browser's alert box.
+ */
+export function greet(): void;
+/**
+ * Generates an array of objects in the form `{x: f64, y: f64}`, with
+ * `x` being the index of the element (from 0 to `n-1`), and `y` being
+ * the sine of `x`. This is useful for generating sample data for
+ * plotting.
+ */
+export function generate_series(n: number): Array<any>;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly greet: () => void;
+  readonly generate_series: (a: number) => any;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
